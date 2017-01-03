704445d40cb207a034bac6a3b2b445da33cb00e6
Ainun Nazieb <ainun@nazieb.com>
2017-01-03 19:22:46 +0700

    add mysql container

---
+++ mysql/mysql.go
@@ -0,0 +1,19 @@
+package mysql
+
+import (
+	"testing"
+	"github.com/KurioApp/gocker"
+	"time"
+	"strconv"
+)
+
+const (
+	containerImage = "mysql"
+	mysqlPort = 33066
+)
+
+func SetupMysqlContainer(t *testing.T) (c gocker.ContainerID, ip string) {
+	return gocker.SetupContainer(t, containerImage, mysqlPort, 10*time.Second, func() (string, error) {
+		return gocker.Run("-d", "-p", strconv.Itoa(mysqlPort) + ":3306", "--name", "mysql-test", "-e", "MYSQL_ROOT_PASSWORD=",containerImage)
+	})
+}
